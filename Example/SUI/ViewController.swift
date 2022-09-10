d1b2c99d31c3778efc2d338ab6660b416e405119
Krishna Venkatramani (Rain Work GPGPassphrase) <krishna@rain.bh>
2022-09-10 10:07:15 +0400

    Initial commit

---
+++ Example/SUI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SUI
+//
+//  Created by 56647167 on 09/10/2022.
+//  Copyright (c) 2022 56647167. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
